1328c94e30e9b50e411ac7266bd45040b1193d34
Tijj Hope <tijjhope@MacBook-Air-Tijj.local>
2025-05-01 17:49:12 +0300

    add .prettierrc

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('Hello, Hexlet!');
+console.log("Hello, Hexlet!");
 
 // По общепринятому соглашению, lodash импортируется под именем _
 import _ from "lodash";
@@ -9,4 +9,4 @@ _.intersection([2, 1], [2, 3]); // => [2]
 // capitalize делает первую букву заглавной
 _.capitalize("hello"); // Hello
 
-console.log(_.last(['one', 'two']));
+console.log(_.last(["one", "two"]));
